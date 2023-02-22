6d914438510795f250a6fe03021d2dcb71a2c92e
Allen <guolong613@gmail.com>
2023-02-22 20:52:09 +0800

    init commit

---
+++ leaf_deploy/dist/assets/TagCreate.5d98f1d6.js
@@ -0,0 +1,1 @@
+import{B as t,M as a}from"./index.c95dd60a.js";import{T as e}from"./TagForm.0e3080c2.js";import{bE as n,bF as o}from"./vendor.28e57c02.js";import"./vant.c4850561.js";const i=n({setup:(r,l)=>()=>o(a,null,{title:()=>"\u65B0\u5EFA\u6807\u7B7E",icon:()=>o(t,null,null),default:()=>o(e,null,null)})});export{i as TagCreate,i as default};
